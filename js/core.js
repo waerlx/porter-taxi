a26388209a38390a95c551f666b488ffa4767c09
waerlx <adelinaakylbekova565@gmail.com>
2024-01-26 18:56:52 +0600

    porter taxi

---
+++ js/core.js
@@ -0,0 +1,23 @@
+// {/* <script>
+// // Get the video
+// var video = document.getElementById("myVideo");
+
+// // Get the button
+// var btn = document.getElementById("myBtn");
+
+// // Pause and play the video, and change the button text
+// function myFunction() {
+//     if (video.paused) {
+//         video.play();
+//         btn.innerHTML = "Pause";
+//     } else {
+//         video.pause();
+//         btn.innerHTML = "Play";
+//     }
+// }
+// </script> */}
+const images = document.querySelectorAll('.main_img');
+
+
+window.addEventListener('scroll', () => {
+    images.style.right = "-" + (window.scrollX / 1.5)+ "px";})
