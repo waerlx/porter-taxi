96c56f869b539af610ab328619bb259140b4d435
waerlx <adelinaakylbekova565@gmail.com>
2024-02-06 21:46:56 +0600

    add swiper and img

--- js/core.js
+++ js/core.js
@@ -35,3 +35,29 @@ for (let smoothLink of smoothLinks) {
         });
     });
 };
+
+let currentIndex = 0;
+  const slides = document.querySelectorAll('.slide');
+  const totalSlides = slides.length;
+
+
+  function showSlide(index) {
+    if (index < 0) {
+      currentIndex = totalSlides - 1;
+    } else if (index >= totalSlides) {
+      currentIndex = 0;
+    } else {
+      currentIndex = index;
+    }
+
+    const transformValue = -currentIndex * 30 + '%';
+    document.querySelector('.slider').style.transform = 'translateX(' + transformValue + ')';
+  }
+
+  function prevSlide() {
+    showSlide(currentIndex - 1);
+  }
+
+  function nextSlide() {
+    showSlide(currentIndex + 1);
+  }
