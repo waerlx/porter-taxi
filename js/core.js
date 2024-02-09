1b5670bcb305b6caa4393feb6ba9f7b36656d5ef
waerlx <adelinaakylbekova565@gmail.com>
2024-02-09 23:16:33 +0600

    remove comment

--- js/core.js
+++ js/core.js
@@ -41,62 +41,6 @@ for (let smoothLink of smoothLinks) {
     });
 };
 
-// let currentIndex = 0;
-//   const slides = document.querySelectorAll('.slide');
-//   const totalSlides = slides.length;
-
-
-//   function showSlide(index) {
-//     if (index < 0) {
-//       currentIndex = totalSlides - 1;
-//     } else if (index >= totalSlides) {
-//       currentIndex = 0;
-//     } else {
-//       currentIndex = index;
-//     }
-
-//     const transformValue = -currentIndex * 30 + '%';
-//     document.querySelector('.slider').style.transform = 'translateX(' + transformValue + ')';
-//   }
-
-//   function prevSlide() {
-//     showSlide(currentIndex - 1);
-//   }
-
-//   function nextSlide() {
-//     showSlide(currentIndex + 1);
-//   }
-
-// $(window).resize(function() {
-//     var window_width = $(window).width();
-//     if (window_width < 600) {
-//         ('.your-class').slick({
-//             dots: true,
-//             slidesToShow:3,
-//             slidesToScroll: 1,
-//             autoplay: true,
-//             autoplaySpeed: 2000,
-//         });
-//     } else {
-//         ('.your-class').slick({
-//             dots: true,
-//             slidesToShow:3,
-//             slidesToScroll: 1,
-//             autoplay: true,
-//             autoplaySpeed: 2000,
-//         });
-//     };
-//   });
-// $('.your-class').slick({
-//     dots: true,
-//     infinite: true,
-//     speed: 300,
-//     slidesToShow: 1,
-//     adaptiveWidth: true,
-//     autoplay: true,
-//     autoplaySpeed: 2000,
-
-//   });
   $('.your-class').slick({
     dots: true,
     infinite: true,
@@ -119,52 +63,13 @@ for (let smoothLink of smoothLinks) {
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          autoplay: true,
-          autoplaySpeed: 2000,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true,
+            autoplay: true,
+            autoplaySpeed: 1500,
         }
-      },
-    //   {
-    //     breakpoint: 480,
-    //     settings: {
-    //       slidesToShow: 1,
-    //       slidesToScroll: 1,
-    //       autoplay: true,
-    //       autoplaySpeed: 2000,
-    //     }
-    //   }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
+      }
     ]
-  });
-  
-
-// $('.your-class').slick({
-//     dots: true,
-//     slidesToShow:3,
-//     slidesToScroll: 1,
-//     autoplay: true,
-//     autoplaySpeed: 2000,
-// });
-// $('.your-class').slick({
-//     dots: true,
-//     infinite: false,
-//     speed: 300,
-//     slidesToShow: 6,
-//     slidesToScroll: 4,
-//     responsive: [
-//       {
-//         breakpoint: 6000,
-//         settings: "unslick"
-//       },
-//       {
-//         breakpoint: 600,
-//         settings: {
-//           slidesToShow: 2,
-//           slidesToScroll: 2
-//         }
-//       }
-//     ]
-//   });
+  })
